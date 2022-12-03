ff8f56055a217dcb8137332f37bc1c1d6532c7ce
floriantoque <florian.toque@gmail.com>
2022-12-03 03:08:36 +0100

    Create page html with mapbox

---
+++ visualization/map.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmxvaGF0MzIiLCJhIjoiY2o1aWhnaXhjMXh6bzMzb2RlamR5N3lxZCJ9.wu2JL5mP4H5eFeDxpmrxNQ';
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-1.32, 46.12], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
