3c4083725a51c9e3af5440f1f4a8c97cea2b5ba5
floriantoque <florian.toque@gmail.com>
2022-12-03 03:10:31 +0100

    Branch add layer fish

--- visualization/map.js
+++ visualization/map.js
@@ -5,5 +5,5 @@ const map = new mapboxgl.Map({
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: [-1.32, 46.12], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    zoom: 10 // starting zoom
 });
